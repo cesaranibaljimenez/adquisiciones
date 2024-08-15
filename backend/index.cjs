b3f3f613b244ece96e14f450c30f3d00d52591e1
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-08-15 15:13:32 -0500

    Integración de variables de entorno y ajustes en el backend

--- backend/index.cjs
+++ backend/index.cjs
@@ -2,9 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require ('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
